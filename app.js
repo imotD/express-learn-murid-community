5cb95e4cdd01b58047ffc68239b3e5985f673636
Tommy Alhamra <tommy.alhamra@ist.id>
2024-10-16 18:40:08 +0700

    feat: registerUser

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const port = 3000;
 
 // Middleware
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Router
 app.use("/api/v1/auth", authRouter);
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,23 @@
-export const RegisterUser = (req, res) => {
-  res.send("Registrasi berhasil");
+import User from "../models/User.js";
+
+export const RegisterUser = async (req, res) => {
+  try {
+    const createUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    return res.status(201).json({
+      message: "Registrasi berhasil",
+      data: createUser,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      message: "Error",
+      error,
+    });
+  }
 };
 
 export const LoginUser = (req, res) => {
