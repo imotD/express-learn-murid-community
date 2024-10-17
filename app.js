0d679ccc51f3e21c6cfba57f34c158e763303d1e
Tommy Alhamra <tommy.alhamra@ist.id>
2024-10-17 17:02:18 +0700

    fix: errorHandler and add jwt token

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import dotnev from "dotenv";
 import cors from "cors";
 import authRouter from "./router/authRouter.js";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import { errorHandler, notFound } from "./middleware/errorHandler.js";
 
 dotnev.config();
 
@@ -13,9 +16,17 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Router
 app.use("/api/v1/auth", authRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 // Endpoint
 app.get("/", (req, res) => {
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,24 +1,40 @@
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import asyncHandler from "../middleware/asyncHandler.js";
 
-export const RegisterUser = async (req, res) => {
-  try {
-    const createUser = await User.create({
-      username: req.body.username,
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    return res.status(201).json({
-      message: "Registrasi berhasil",
-      data: createUser,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      message: "Error",
-      error,
-    });
-  }
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_TOKEN, {
+    expiresIn: "6d",
+  });
 };
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user.id);
+
+  const cookieOption = {
+    expire: new Date(Date.now() + 6 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    security: false,
+  };
+
+  res.cookie("jwt", token, cookieOption);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    data: user,
+  });
+};
+
+export const RegisterUser = asyncHandler(async (req, res) => {
+  const createUser = await User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  createSendToken(createUser, 201, res);
+});
 
 export const LoginUser = (req, res) => {
   res.send("Login berhasil");
